4ff828fe3ec6a47d471899e5460fe57863194995
jinwon93 <kjw8208@nate.com>
2021-08-06 23:28:40 +0900

    fc-react-jwiter

--- src/jbase.js
+++ src/jbase.js
@@ -11,5 +11,7 @@ const firebaseConfig = {
   };
   
   firebase.initializeApp(firebaseConfig);
+  
+  export const firebaseInstance = firebase;
 
   export const authService = firebase.auth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { authService } from "jbase";
+import { firebaseInstance } from './../jbase';
 
 
 const Auth = () => {
@@ -36,7 +37,19 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
-
+  const onSocialClick = async (event) => {
+    const {
+      target : {name},
+    } =event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }else if (name === "github"){
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    await authService.signInWithPopup(provider);
+    
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -66,8 +79,8 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
       </div>
     </div>
   );
