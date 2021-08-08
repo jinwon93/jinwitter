fb2eb13c04c90367351c21bc508104a5e5b15249
jinwon93 <kjw8208@nate.com>
2021-08-08 23:12:06 +0900

    react-jweet-4

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,61 @@
 import React from "react";
+import { useState , useEffect } from "react";
+import { dbService } from "jbase";
 
+const Home = () => {
+  const [jweet, setJweet] = useState("");
+  const [jweets, setJweets] = useState([]);
+  const getJweets = async () => {
+    const dbJweets = await dbService.collection("jweets").get();
+    
+    dbJweets.forEach((document) => {
+        const jweetObject ={
+            ...document.data(),
+            id:document.id,
+        };
+      setJweet((prev) => [jweetObject, ...prev]);
 
-const Home = () => <span>Home</span>;
+    });
+  };
+  
+  useEffect(() => {
+    getJweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("jweet").add({
+      jweet,
+      createdAt: Date.now(),
+    });
+    setJweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setJweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={jweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Jweet" />
+      </form>
+      <div>
+          {jweets.map((jweet)=>(
+              <div key={jweet.id}>
+                  <h4>{jweet.jweet}</h4>
+              </div>    
+          ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
